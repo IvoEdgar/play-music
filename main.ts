3cf79fc392e1362edfae755deb96ecd7e8a58484
Ivo Edgar <ivo.edgy@gmail.com>
2021-10-07 01:21:25 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,19 +1,19 @@
 input.onButtonPressed(Button.A, function () {
     basic.showString("A")
-    music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
+    music.startMelody(music.builtInMelody(Melodies.Ode), MelodyOptions.Once)
     basic.clearScreen()
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showString("C")
-    music.startMelody(music.builtInMelody(Melodies.Funeral), MelodyOptions.Once)
+    music.startMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once)
     basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
     basic.showString("B")
-    music.startMelody(music.builtInMelody(Melodies.Wedding), MelodyOptions.Once)
+    music.startMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Once)
     basic.clearScreen()
 })
-music.setVolume(255)
+music.setVolume(96)
 for (let index = 0; index < 2; index++) {
     basic.showIcon(IconNames.Heart)
     basic.showIcon(IconNames.SmallHeart)
